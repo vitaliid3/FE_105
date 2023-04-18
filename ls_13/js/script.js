ba5b53f53e2700446544feb986538e9dfeba7c0b
Your Name <you@example.com>
2023-04-18 21:42:45 +0300

    Team work.

--- ls_13/js/script.js
+++
@@ -1,7 +0,0 @@
-let burger = document.getElementsByClassName('burger')[0];
-let menu = document.getElementsByClassName('main-menu')[0];
-
-burger.addEventListener("click", function(e) {
-  burger.classList.toggle('burger_active')
-  menu.classList.toggle('main-menu_active')
-}, false);
